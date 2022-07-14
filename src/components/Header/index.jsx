c91f3426acff85cffb6e89227e33d5cfe8095bce
StepanT1005 <stkdqpuu@gmail.com>
2022-07-14 19:21:05 +0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+
+export const Header = () => {
+  const dispatch = useDispatch();
+
+  const isAuth = useSelector(selectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm("Ви впевнені, що бажаєте вийти?")) {
+      dispatch(logout());
+      window.localStorage.removeItem("token");
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>Coctails blog</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/add-post">
+                  <Button variant="contained">Створити статтю</Button>
+                </Link>
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Вийти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,164 @@
+import React, { useEffect, useState, useRef } from "react";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import SimpleMDE from "react-simplemde-editor";
+import { useSelector } from "react-redux";
+import { useParams, useNavigate, Navigate, Link } from "react-router-dom";
+
+import "easymde/dist/easymde.min.css";
+import styles from "./AddPost.module.scss";
+import { selectIsAuth } from "../../redux/slices/auth";
+import axios from "../../axios";
+
+export const AddPost = () => {
+  const { id } = useParams();
+  const isEditing = Boolean(id);
+  const navigate = useNavigate();
+  const isAuth = useSelector(selectIsAuth);
+  const [isLoading, setLoading] = useState();
+
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState("");
+  const [imageUrl, setImageUrl] = useState();
+  const inputFileRef = useRef(null);
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+
+      const { data } = await axios.post("/upload", formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert("Помилка при завантаженні файла");
+    }
+  };
+  const onClickRemoveImage = () => {
+    setImageUrl("");
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  useEffect(() => {
+    if (id) {
+      axios.get(`/posts/${id}`).then(({ data }) => {
+        setTitle(data.title);
+        setText(data.text);
+        setTags(data.tags.join(","));
+        setImageUrl(data.imageUrl);
+      });
+    }
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: "400px",
+      autofocus: true,
+      placeholder: "Введіть текст...",
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    []
+  );
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+      const fields = {
+        title,
+        text,
+        imageUrl,
+        tags,
+      };
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, fields)
+        : await axios.post("/posts", fields);
+
+      const _id = isEditing ? id : data._id;
+      navigate(`/posts/${_id}`);
+    } catch (err) {
+      console.warn(err);
+      alert("Помилка при створенні статті");
+    }
+  };
+
+  if (!window.localStorage.getItem("token") && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
+        Загрузити фото
+      </Button>
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Видалити
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Заголовок статті..."
+        fullWidth
+      />
+      <TextField
+        classes={{ root: styles.tags }}
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        variant="standard"
+        placeholder="Теги"
+        fullWidth
+      />
+      <SimpleMDE
+        className={styles.editor}
+        value={text}
+        onChange={onChange}
+        options={options}
+      />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? "Зберегти" : "Опублікувати"}
+        </Button>
+        <Link to="/">
+          <Button size="large">Відміна</Button>
+        </Link>
+      </div>
+    </Paper>
+  );
+};
