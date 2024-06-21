41c389bffa673d9b2dc246aadb2bd61dd4e80c4a
StepanT1005 <stkdqpuu@gmail.com>
2024-06-21 10:41:05 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
   },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
