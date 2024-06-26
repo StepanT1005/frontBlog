56c7886b335b63e6b2d7f5476a27fcc32511dd42
StepanT1005 <stkdqpuu@gmail.com>
2024-06-26 09:46:54 +0200

    update url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 export const DEFAULT_AVATAR = BASE_URL + "/uploads/default-avatar.png";
 
 // export type ApiErrors = {
